9e264aba493cb1a562274b440ad82c6be75d53b1
Táňa Zimmermannová <tana.zimmermannova@seznam.cz>
2022-12-16 22:49:39 +0100

    final

---
+++ .eleventy.js
@@ -0,0 +1,24 @@
+module.exports = function(eleventyConfig) {
+
+	// Výchozí výstupní složka: _site
+
+	// Zkopírovat images/ do _site/images
+	eleventyConfig.addPassthroughCopy("images");
+
+	// Zkopírovat CSS/ to _site/css/
+	eleventyConfig.addPassthroughCopy("CSS");
+
+  // Zkopírovat JS/ to _site/JS/
+	eleventyConfig.addPassthroughCopy("JS");
+
+  return {
+    // možné formáty šablon
+    templateFormats: ["njk", "html", "md", "liquid"],
+
+    // jako šablonovací jazyk zvolíme Nunjucks
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
+    dataTemplateEngine: "njk",
+  }
+
+};
